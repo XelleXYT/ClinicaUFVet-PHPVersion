f232aeab841408da8729ffa7427d6778f5b10dc3
Marta <Marta@172.16.6.107>
2018-11-14 18:05:40 +0100

    Primera página creada. Incluidas carpetas de estilos y js/jQuery y agregada una hoja de estilos CSS que de momento está vacía.

--- index.php
+++ index.php
@@ -8,10 +8,39 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/estilosVet.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="banner">Aquí va el banner</div>
+        <div class="contenido">
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-6">
+                             <input id="cajanombre" class="form-control" name="usuario_nombre" type="text" placeholder="Usuario">
+                             <br>                                                                                      <!--Podemos llamar a una variable php dentro de un tag html-->
+                             <input id="cajapassword" class="form-control" name="contraseña" type="password" placeholder="Contraseña">
+                             <br>
+                             <button type="submit" class="btn btn-primary" id="boton1">Login</button> 
+                        </div>
+                        <div class="col-6"></div>
+                    </div>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>
+        
+        
+        
     </body>
+    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+    
+        <script>
+        
+        
+        </script>
+    
+}
 </html>
