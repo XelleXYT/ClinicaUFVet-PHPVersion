35ee42dabbfe2f01387246b21f02b5617113868a
Marta <Marta@172.16.6.107>
2018-11-21 18:43:22 +0100

    Botones totalmente implementados.

--- PaginaPrincipal.php
+++ PaginaPrincipal.php
@@ -1,12 +1,15 @@
 <?php
 ?>
 <div id="menu" class="row">
-    <div class="col-md-6">
-        <button type="submit" class="btn btn-primary botones" id="boton1" onclick="cargaMascota()">Mascota</button>
+    <div class="col-md-4">
+        <button type="submit" class="btn btn-primary botonMascota" id="boton1" onclick="cargaMascota()"></button>
     </div>
-    <div class="col-md-6">
-        <button type="submit" class="btn btn-primary botones" id="boton1" onclick="cargaDueno()">Dueño</button>
+    <div class="col-md-4">
+        <button type="submit" class="btn btn-primary botonCliente" id="boton1" onclick="cargaDueno()"></button>
     </div>  
+    <div class="col-md-4">
+        <button type="submit" class="btn btn-primary botonBuscar" id="boton1" onclick=""></button>
+    </div>
 </div>
 <div class="row" style="height: 20px; width: 1000px; background-color: #2f3145;"></div>
 <div>
